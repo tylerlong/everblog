e894dc97306c4a6197e41367434e0702b6eb9c79
Tyler Long <tyler4long@gmail.com>
2012-01-14 06:19:04 -0800

    fetches created time and updated time from Evernote

--- everblog/models.py
+++ everblog/models.py
@@ -4,19 +4,20 @@
     ~~~~~~~~~~~~~~~
     Sqlalchemy database models
 """
-import re, urllib2
+import re, urllib2, json, datetime
 from quick_orm.core import Database
 from sqlalchemy import Column, String, Text, DateTime, func, Integer, Enum
 from toolkit_library.text_converter import TextConverter
 
 
-evernote_url_pattern = re.compile('\.evernote\.com/shard/(?P<shard_id>[a-z]{1,2}\d{1,3})/sh/(?P<guid>[a-z0-9-]{24,48})/(?P<key>[a-z0-9]{24,40})')
-evernote_title_pattern = re.compile('<title>(.+?)</title>')
+evernote_data_pattern = re.compile('(?<=Evernote\.WebClient\.Note = )\{.+?\}(?=;\s+?</script>)')
 chinese_character_pattern = re.compile(u'[\u4e00-\u9fa5]')
 
 
 class DefaultModel(object):
-    created = Column(DateTime, default = func.now())
+    created = Column(DateTime, nullable = False)
+    updated = Column(DateTime, nullable = False)
+    published = Column(DateTime, default = func.now())
     
 metaclass = Database.MetaBuilder(DefaultModel)
 
@@ -30,11 +31,12 @@ class Article(object):
 
     def synchronize(self):
         data = urllib2.urlopen(self.evernote_url).read().decode('UTF-8')
-        self.title = evernote_title_pattern.search(data).group(1)
-
-        dict_ = evernote_url_pattern.search(self.evernote_url).groupdict()
-        url = 'https://www.evernote.com/shard/{0}/sh/{1}/{2}/note/{1}'.format(dict_['shard_id'], dict_['guid'], dict_['key'])
-        self.content = urllib2.urlopen(url).read().decode('UTF-8')
+        data = evernote_data_pattern.search(data).group()
+        dict_ = json.loads(data.replace('\<', '<').replace('\>', '>'))
+        self.title = dict_['title']
+        self.content = dict_['content']
+        self.created = datetime.datetime.fromtimestamp(dict_['created']/1000)
+        self.updated = datetime.datetime.fromtimestamp(dict_['updated']/1000)
 
 
 class BlogEntry(Article):
