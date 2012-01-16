2a4d948c46ff0ae2965a6555063ba478558c0319
Tyler Long <tyler4long@gmail.com>
2012-01-16 06:27:14 -0800

    added wsgi.py

---
+++ everblog/wsgi.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+"""
+    everblog.wsgi
+    ~~~~~~~~~~~~~
+    To be invoked by a web server which supports wsgi
+"""
+VIRTUALENV_PATH = '/srv/envs/prod/'
+PROJECT_PATH = '/srv/www/everblog/'
+
+import os
+# Activate virtualenv
+activate_this = os.path.join(VIRTUALENV_PATH, 'bin/activate_this.py')
+execfile(activate_this, dict(__file__ = activate_this))
+
+import sys
+# add project folder to sys.path
+if PROJECT_PATH not in sys.path:
+    sys.path.append(PROJECT_PATH)
+
+# mod_wsgi requires a variable named application
+from everblog import app as application
